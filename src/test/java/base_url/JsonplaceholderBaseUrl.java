3c8e6d6f4451759c7fd2f6882d30a8cd00397cf5
Zekeriya Bozkurt <bozkurt.zekeriya@gmail.com>
2022-10-27 15:01:02 +0300

    27/10/2022

--- src/test/java/base_url/JsonplaceholderBaseUrl.java
+++ src/test/java/base_url/JsonplaceholderBaseUrl.java
@@ -1,3 +1,5 @@
+package base_url;
+
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
@@ -8,5 +10,6 @@ public class JsonplaceholderBaseUrl {
    @Before
    public void setup(){
        spec=new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
+
    }
 }
